7c7944e9d57dce15ff6aa2587f720e04e17483e1
Chia Pak Mun <pakmunchia@ifca.com.my>
2020-11-24 16:04:18 +0800

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import swDev from './swDev';
 
