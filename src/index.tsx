d72d72ee7787823be6802f72083dc003cef633db
Chia Pak Mun <pakmunchia@ifca.com.my>
2020-11-24 16:04:27 +0800

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import swDev from './swDev';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
